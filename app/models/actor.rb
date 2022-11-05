7a85675e0f6ad916aac76aa87dd8a8901b0d7d36
jeffoty <odhiambo47@gmail.com>
2022-11-06 02:45:50 +0300

    done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows
+
+    def full_name
+        self.first_name.last_name
+    end
+
+    def list_roles
+        self.characters.shows
+    end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    belongs_to :network
+    has_many :actors
+
+    def actors_list
+        actors.first_name.last_name
+
+    end
   
 end
